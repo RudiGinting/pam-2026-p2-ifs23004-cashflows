918faf43ef40bc7b935a2e4e606cb9f434cef8b0
RudiGinting <rudialvajonathanginting@gmail.com>
2026-02-16 11:32:59 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "2.3.0"
+    id("io.ktor.plugin") version "3.3.2"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+val kotlin_version: String by project
+val ktor_version: String by project
+val koin_version: String by project
+val logback_version: String by project
+val dotenv_version: String by project
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-config-yaml-jvm:$ktor_version")
+
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_version")
+
+
+    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "pam-2026-p2-ifs23004-cashflows"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
